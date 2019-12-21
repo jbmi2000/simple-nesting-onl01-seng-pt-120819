16b8cee311ec2c72c85c48854588f1c2754f025d
Jeffrey Marbury <jeff.marbury@gmail.com>
2019-12-21 16:45:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -40,7 +40,10 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
+
+
 
 programmer_hash = {:alan_kay_is_known_for => "Object Orientation"}
 
