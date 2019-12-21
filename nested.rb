822b71171945cfe58f3a312091aa18f8dda8b096
Jeffrey Marbury <jeff.marbury@gmail.com>
2019-12-21 17:08:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -90,7 +90,6 @@ def adding_matz
 #programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 end
-programmer_hash
 
 
 def changing_alan
