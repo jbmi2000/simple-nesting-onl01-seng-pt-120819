fad61e17289a03d1f6dd859bd3271f1853d7bcb3
Jeffrey Marbury <jeff.marbury@gmail.com>
2019-12-21 15:53:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-programmer_hash = {:grace_hopper =>{:hopper => :known_for => "COBOL", :languages => ["COBOL","FORTRAN"]
+programmer_hash = {:grace_hopper =>{:hopper => :known_for => "COBOL" :languages => ["COBOL","FORTRAN"]
 
 end
 
